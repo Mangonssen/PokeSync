21f18c7f3c20575394f1e62dc6fc6b5bd7639230
LPTimey <86461861+LPTimey@users.noreply.github.com>
2026-08-19 21:16:49 +0200

    Ref for Pixel & web components example idea

---
+++ src/components/battle-button.mjs
@@ -0,0 +1,14 @@
+/** @typedef {"left"|"mid"|"right"} BBState   */
+
+export class BattleButton extends HTMLElement { 
+    /**
+     * 
+     * @param {BBState} [state] 
+     */
+    constructor(state){
+        super();
+        this.state = state??this.dataset.state??"left";
+    }
+}
+customElements.define("battle-button", BattleButton);
+
