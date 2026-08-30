c4c83ef7dfd65c535bee17c398a61d8c4f70c2cd
LPTimey <86461861+LPTimey@users.noreply.github.com>
2026-08-30 15:54:56 +0200

    Update settings-line.mjs
    but this feels very unwieldy, need diffrent impl

--- src/components/settings-line.mjs
+++ src/components/settings-line.mjs
@@ -13,6 +13,10 @@ import { assertNever, css, html } from "../utils.mjs";
  */
 
 /**
+ * @typedef {SettingsLineState & {title:string}} SettingsLineParams
+ */
+
+/**
  * @param {SettingsLineState} state
  * @returns {string}
  */
@@ -33,13 +37,15 @@ const HTML = (state) => {
         } break;
 
         case "binary": {
+            const text1Active = state.text1Active ?? true;
+
             specific = html`
                 <div class="binary">
                     <label>
                         <input
                             type="radio"
                             name="binary"
-                            ${(state.text1Active ?? true) ? "checked" : ""}
+                            ${text1Active ? "checked" : ""}
                         />
                         <span>${state.text1}</span>
                     </label>
@@ -48,7 +54,7 @@ const HTML = (state) => {
                         <input
                             type="radio"
                             name="binary"
-                            ${(state.text1Active ?? true) ? "" : "checked"}
+                            ${text1Active ? "" : "checked"}
                         />
                         <span>${state.text2}</span>
                     </label>
@@ -116,7 +122,6 @@ const HTML = (state) => {
                     </span>
 
                     <div class="file-button">
-                        <!-- <img src="./assets/upload.svg" /> -->
                         <input type="file" />
                     </div>
                 </label>
@@ -203,24 +208,21 @@ const CSS = css`
         align-items: stretch;
         align-content: stretch;
 
-        &::before{
+        &::before {
             display: block;
             flex: 1 1 0;
-            content:'';
+            content: '';
             /* TODO: variable */
             background-color: #4A5252;
         }
-        &::after{
-            /* content: url("assets/arrow head.svg"); */
+
+        &::after {
             content: '';
             flex: 0 0 auto;
             aspect-ratio: 12 / 30;
             background: url("assets/arrow head.svg");
             background-size: cover;
             margin-left: -2px;
-
-            /* TODO: variable */
-            fill: #4A5252;
         }
     }
 }
@@ -228,32 +230,37 @@ const CSS = css`
 .content {
     flex: 1 1 0;
     position: relative;
-    .underline::after{
-        content:'';
+
+    .underline::after {
+        content: '';
         position: absolute;
         left: 0;
         right: 0;
         bottom: 0;
         height: 0.075em;
-        /* TODO: variable */
         background-color: #42DEE7;
     }
-    .string{
-        input{
+
+    .string {
+        input {
             width: -webkit-fill-available;
             width: stretch;
             background: transparent;
             border: none;
+            outline: none;
+            color: inherit;
             font: inherit;
         }
     }
-    .binary,.triple{
+
+    .binary,
+    .triple {
         display: flex;
         align-items: center;
         justify-content: space-around;
         justify-content: space-evenly;
 
-        input{
+        input {
             appearance: none;
             width: 0.5ch;
             height: 0.5em;
@@ -261,36 +268,50 @@ const CSS = css`
             background-color: #73debd;
             /* TODO: variable */
             border: 0.05em solid #212129;
+            cursor: pointer;
         }
-        label{
+
+        label {
             text-transform: uppercase;
-            &:has(:checked){
+            cursor: pointer;
+
+            &:has(:checked) {
                 /* TODO: variable */
                 color: #42DEE7;
                 filter:
                     /* TODO: variable */
                     drop-shadow(0px 0.05em 0px #318494)
                     /* TODO: variable */
-                    drop-shadow(0.05em 0px 0px #318494)
-                    ;
+                    drop-shadow(0.05em 0px 0px #318494);
             }
-            &:not(:has(:checked)){
+
+            &:not(:has(:checked)) {
                 /* TODO: variable */
                 color: #7b9c9c;
                 filter:
-                    /* TODO: variable */
+                /* TODO: variable */
                     drop-shadow(0px 0.05em 0px #426b7b)
-                    /* TODO: variable */
-                    drop-shadow(0.05em 0px 0px #426b7b)
-                    ;
+                /* TODO: variable */
+                    drop-shadow(0.05em 0px 0px #426b7b);
             }
         }
     }
-    .select{
+
+    select {
+        appearance: auto;
         appearance: base-select;
         width: -webkit-fill-available;
         width: stretch;
+        background: transparent;
+        border: none;
+        outline: none;
+        /* TODO: variable */
+        color: #42DEE7;
+        font: inherit;
+        cursor: pointer;
+        text-transform: uppercase;
     }
+
     .file {
         display: flex;
         align-items: center;
@@ -312,22 +333,22 @@ const CSS = css`
                 /* TODO: variable */
                 drop-shadow(0px 0.05em 0px #318494)
                 /* TODO: variable */
-                drop-shadow(0.05em 0px 0px #318494)
-                ;
+                drop-shadow(0.05em 0px 0px #318494);
             text-transform: uppercase;
         }
 
         .file-button {
             display: flex;
-            align-items:center;
-            justify-content:center;
+            align-items: center;
+            justify-content: center;
             flex: 0 0 auto;
+
             filter:
                 /* TODO: variable */
                 drop-shadow(0px 0.05em 0px #318494)
                 /* TODO: variable */
-                drop-shadow(0.05em 0px 0px #318494)
-                ;
+                drop-shadow(0.05em 0px 0px #318494);
+
             &::before {
                 content: "";
                 display: inline-block;
@@ -346,33 +367,37 @@ const CSS = css`
             display: none;
         }
     }
-    .submenu{
+
+    .submenu {
         width: -webkit-fill-available;
         width: stretch;
         border: none;
         background-color: transparent;
         text-decoration: none;
         display: flex;
-        align-items:center;
+        align-items: center;
         justify-content: space-between;
         padding-inline: 2ch;
         text-transform: uppercase;
         /* TODO: variable */
         color: #42DEE7;
+        font: inherit;
+        cursor: pointer;
+
         filter:
             /* TODO: variable */
             drop-shadow(0px 0.05em 0px #318494)
             /* TODO: variable */
-            drop-shadow(0.05em 0px 0px #318494)
-            ;
+            drop-shadow(0.05em 0px 0px #318494);
+
+        .arrow {
+            font-size: 1.5em;
+            line-height: 0.5;
+        }
     }
 }
 `;
 
-
-/**
- * @typedef {SettingsLineState} SettingsLineParams
- */
 
 export class SettingsLine extends HTMLElement {
     /**
@@ -381,10 +406,206 @@ export class SettingsLine extends HTMLElement {
     constructor(params) {
         super();
 
-        this.innerHTML = "Settings";
+        /** @type {SettingsLineParams} */
+        this.state = params ?? {
+            title: "Setting",
+            kind: "select",
+            options: ["test", "test2", "test3", "test4"],
+        };
 
-        const shadowRoot = this.attachShadow({ mode: "open" });
-        shadowRoot.innerHTML = CSS + HTML(params ?? { kind: "select", options: ["test", "test2", "test3", "test4",] });
+        this.innerHTML = this.state.title || this.title || "Setting";
+
+        /** @type {ShadowRoot} */
+        this.shadow = this.attachShadow({ mode: "open" });
+
+        this.render();
+    }
+
+    render() {
+        this.shadow.innerHTML =
+            CSS + HTML(this.state);
+
+        this.setupEvents();
+    }
+
+    setupEvents() {
+        /** @type {HTMLDivElement|null} */
+        const content = this.shadow.querySelector(".content");
+        const state = this.state;
+        const kind = state.kind;
+
+        if (!content) return;
+
+        switch (kind) {
+            case "string": {
+                const input = content.querySelector("input");
+
+                input?.addEventListener("input", () => {
+                    state.content = input.value;
+
+                    this.emitChange(input.value);
+                });
+            } break;
+
+            case "binary": {
+                /** @type {NodeListOf<HTMLInputElement>} */
+                const inputs = content.querySelectorAll(
+                    'input[type="radio"]'
+                );
+
+                inputs.forEach((input) => {
+                    input.addEventListener("change", () => {
+                        const active = Number(input.value);
+
+                        state.text1Active = active === 0;
+
+                        this.emitChange(active);
+                    });
+                });
+            } break;
+
+            case "triple": {
+                /** @type {NodeListOf<HTMLInputElement>} */
+                const inputs = content.querySelectorAll(
+                    'input[type="radio"]'
+                );
+
+                inputs.forEach((input) => {
+                    input.addEventListener("change", () => {
+                        const active = Number(input.value);
+
+                        state.active = active;
+
+                        this.emitChange(active);
+                    });
+                });
+            } break;
+
+            case "select": {
+                const select = content.querySelector("select");
+
+                select?.addEventListener("change", () => {
+                    const active = Number(select.value);
+
+                    state.active = active;
+
+                    this.emitChange({
+                        index: active,
+                        value: state.options[active],
+                    });
+                });
+            } break;
+
+            case "file": {
+                /** @type {HTMLInputElement|null} */
+                const input = content.querySelector(
+                    'input[type="file"]'
+                );
+                /** @type {HTMLElement|null} */
+                const fileName = content.querySelector(".file-name");
+
+                input?.addEventListener("change", () => {
+                    const file = input.files?.[0];
+
+                    if (!file) return;
+                    if (!fileName) return;
+
+                    fileName.textContent = file.name;
+
+                    state.path = file.name;
+
+                    this.emitChange(file);
+                });
+            } break;
+
+            case "submenu": {
+                if ("callback" in state) {
+                    const button = content.querySelector("button");
+
+                    button?.addEventListener("click", () => {
+                        state.callback();
+                        this.emitChange();
+                    });
+                }
+            } break;
+
+            default: {
+                assertNever(kind);
+            }
+        }
+    }
+
+    /**
+     * Dispatch a public change event.
+     *
+     * @param {*} value
+     */
+    emitChange(value = this.value) {
+        this.dispatchEvent(
+            new CustomEvent("change", {
+                detail: {
+                    value,
+                    state: this.state,
+                },
+                bubbles: true,
+                composed: true,
+            })
+        );
+    }
+
+    /**
+     * Current value of the control.
+     */
+    get value() {
+        const state = this.state;
+        const kind = state.kind;
+        switch (kind) {
+            case "string":
+                return state.content ?? "";
+
+            case "binary":
+                return state.text1Active ?? true
+                    ? 0
+                    : 1;
+
+            case "triple":
+                return state.active ?? 0;
+
+            case "select": {
+                const index = state.active ?? 0;
+
+                return {
+                    index,
+                    value: state.options[index],
+                };
+            }
+
+            case "file":
+                return state.path ?? null;
+
+            case "submenu":
+                return undefined;
+
+            default:
+                return assertNever(kind);
+        }
+    }
+
+    /**
+     * Replace the component state and re-render.
+     *
+     * @param {SettingsLineParams} state
+     */
+    setState(state) {
+        this.state = state;
+        this.render();
+    }
+
+    /**
+     * Current component state.
+     */
+    getState() {
+        return this.state;
     }
 }
 
