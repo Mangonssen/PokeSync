d2431655615e1ea738f7ea69e78fb08ae588dc09
LPTimey <86461861+LPTimey@users.noreply.github.com>
2026-09-02 17:38:59 +0200

    functional select

--- src/components/settings-line/verions/select.mjs
+++ src/components/settings-line/verions/select.mjs
@@ -2,29 +2,54 @@ import { css, html } from "../../../utils.mjs";
 import { commonHTML, commonCSS } from "../common.mjs";
 
 export class SLSelect extends HTMLElement {
+    static formAssociated = true;
     static observedAttributes = [
         "name",
         "title",
         "placeholder",
+        "required",
+        "disabled",
     ];
 
     constructor() {
         super();
 
+        this.internals_ = this.attachInternals();
         this.shadow = this.attachShadow({ mode: "open" });
-
-        this.name = this.getAttribute("name") ?? crypto.randomUUID();
-        this.placeholder = this.getAttribute("placeholder");
     }
 
     get HTML() {
-        return commonHTML(this.title, html`
-            <select name="${this.name}">
-                ${this.placeholder ? `<option value="">-- ${this.placeholder} --</option>` : ""}
-                ${this.innerHTML}
-            </select>
-        `);
+        const select = document.createElement("select");
+
+        if (this.placeholder) {
+            select.add(new Option(`-- ${this.placeholder} --`, ""));
+        }
+
+        for (const option of this.querySelectorAll("option")) {
+            const clone = /** @type {HTMLOptionElement} */(option.cloneNode(true));
+
+            if (clone.value === this.value) {
+                clone.setAttribute("selected", "");
+            }
+
+            select.appendChild(clone);
+        }
+
+        if (this.name) {
+            select.name = this.name;
+        }
+
+        if (this.required) {
+            select.required = true;
+        }
+
+        if (this.disabled) {
+            select.disabled = true;
+        }
+
+        return commonHTML(this.title, select.outerHTML);
     }
+
 
     get CSS() {
         return commonCSS + css`
@@ -36,6 +61,7 @@ export class SLSelect extends HTMLElement {
                 background: transparent;
                 border: none;
                 outline: none;
+                text-align: center;
                 /* TODO: variable */
                 color: #42DEE7;
                 font: inherit;
@@ -46,6 +72,9 @@ export class SLSelect extends HTMLElement {
     }
 
     connectedCallback() {
+        if (this.name) {
+            this.value = new URLSearchParams(window.location.search).get(this.name);
+        }
         this.render();
     }
 
@@ -61,35 +90,130 @@ export class SLSelect extends HTMLElement {
             return;
         }
 
-        if (name === "name") {
-            this.name = newValue ?? "";
-            if (this.select) {
+        this.internals_.setFormValue(this.value ?? "");
+
+        if (!this.select) {
+            return;
+        }
+
+        switch (name) {
+            case "name": {
                 this.select.name = newValue ?? "";
-            }
+            } break;
+            case "value": {
+                this.select.value = newValue ?? "";
+            } break;
+            case "placeholder": {
+                let placeholderOption = this.select?.querySelector("option[value='']");
+                if (placeholderOption) {
+                    placeholderOption.innerHTML = `-- ${newValue} --`;
+                } else if (this.select) {
+                    this.render()
+                }
+            } break;
+            case "required": {
+                this.select.required = newValue !== null;
+            } break;
+            case "disabled": {
+                this.select.disabled = newValue !== null;
+            } break;
+            case "title": {
+                if (this.titleEl) {
+                    this.titleEl.innerText = newValue ?? "";
+                }
+            } break;
         }
 
-        if (name === "title") {
-            this.title = newValue ?? "";
-            if (this.titleEl) {
-                this.titleEl.innerText = newValue;
-            }
-        }
-
-        if (name === "placeholder") {
-            this.placeholder = newValue ?? "";
-            let placeholderOption = this.select?.querySelector("option[value='']");
-            if (placeholderOption) {
-                placeholderOption.innerHTML = `-- ${newValue} --`;
-            } else if (this.select) {
-                this.render()
-            }
-        }
+        this.#syncValidity();
     }
 
     render() {
         this.shadow.innerHTML = this.CSS + this.HTML;
         this.titleEl = /** @type {HTMLSpanElement} */(this.shadow.querySelector(".title"));
         this.select = /** @type {HTMLSelectElement} */(this.shadow.querySelector("select"));
+        this.select.addEventListener("change", this.#onChange)
+        this.#syncValidity();
+    }
+
+    #onChange = () => {
+
+        if (!this.select) {
+            this.internals_.setFormValue(null);
+            this.#syncValidity();
+            return;
+        }
+
+        this.internals_.setFormValue(this.select.value);
+        this.value = this.select.value;
+
+        this.#syncValidity();
+
+        this.dispatchEvent(
+            new Event("change", {
+                bubbles: true,
+                composed: true,
+            }),
+        );
+    };
+    #syncValidity() {
+        if (!this.select) {
+            return;
+        }
+
+        const { validity, validationMessage } = this.select;
+
+        if (validity.valid) {
+            this.internals_.setValidity({});
+        } else {
+            this.internals_.setValidity(
+                validity,
+                validationMessage || 'Please enter a valid file.',
+                this.select
+            );
+        }
+    }
+
+    get name() {
+        return this.getAttribute("name");
+    }
+    set name(value) {
+        if (value) {
+            this.setAttribute("name", value);
+        } else {
+            this.removeAttribute("name");
+        }
+    }
+    get value() {
+        return this.getAttribute("value");
+    }
+    set value(value) {
+        if (value) {
+            this.setAttribute("value", value);
+        } else {
+            this.removeAttribute("value");
+        }
+    }
+    get placeholder() {
+        return this.getAttribute("placeholder");
+    }
+    set placeholder(value) {
+        if (value) {
+            this.setAttribute("placeholder", value);
+        } else {
+            this.removeAttribute("placeholder");
+        }
+    }
+    get required() {
+        return this.hasAttribute("required");
+    }
+    set required(val) {
+        this.toggleAttribute("required", !!val);
+    }
+    get disabled() {
+        return this.hasAttribute("disabled");
+    }
+    set disabled(val) {
+        this.toggleAttribute("disabled", !!val);
     }
 }
 
